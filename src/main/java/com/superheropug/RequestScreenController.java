07372b3026552a1fcaf58953dbedea0ad7c20673
superheropug <71048116+superheropug@users.noreply.github.com>
2024-03-07 19:04:44 -0600

    Stable now, I should really stop commiting to Main now.

--- src/main/java/com/superheropug/RequestScreenController.java
+++ src/main/java/com/superheropug/RequestScreenController.java
@@ -5,18 +5,29 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.superheropug.Service.CurlVersion;
+import com.superheropug.Service.RequestSender;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 public class RequestScreenController{
     @FXML
     private ChoiceBox<String> httpMethod;
     @FXML
     private Label curlVersion;
-
+    @FXML
+    private Label responseField;
+    @FXML
+    private Button sendButton;
+    @FXML
+    private TextField urlField;
+    @FXML
+    private ScrollPane scrollPane;
 
     private String[] httpMethods = {"GET","POST","PUT","PATCH","DELETE","HEAD"};
     @FXML
@@ -27,5 +38,16 @@ public class RequestScreenController{
     public void initialize(){
         try{curlVersion.setText("Curl Installed : "+CurlVersion.getCurlVersion());} catch(Exception e){e.printStackTrace();}
         httpMethod.getItems().addAll(httpMethods);
+        scrollPane.setContent(responseField);
+    }
+    @FXML
+    public void cUrl(){
+        String response = RequestSender.getSender().sendRequest(urlField.getText(), httpMethod.getValue());
+        responseField.setText(response);
+        
+    }
+    @FXML
+    public void scrollDown(){
+        scrollPane.setVvalue(scrollPane.getVvalue()+0.01);
     }
 }
--- src/main/java/com/superheropug/Service/RequestSender.java
+++ src/main/java/com/superheropug/Service/RequestSender.java
@@ -1,8 +1,52 @@
 package com.superheropug.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class RequestSender {
     // This should be the smallest stub possible
+    // Singleton design
     String hostStub;
     String endpoint;
+    static RequestSender sender = new RequestSender();
+    private RequestSender(){
 
+    }
+    public static RequestSender getSender(){
+        return sender;
+    }
+    public String sendRequest(String url, String method){
+        /*ProcessBuilder pb = new ProcessBuilder("cmd.exe","/c","curl -V");
+        Process p = pb.start();
+        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        line = br.readLine();
+        while (line != null && ! line.contains("curl")){
+            line = br.readLine();
+        }
+        br.close(); */
+        String ret = "";
+        try{
+            ProcessBuilder pb = new ProcessBuilder("cmd.exe","/c",assembleCurlCommand(url, method));
+            Process p = pb.start();
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String next;
+            next = br.readLine();
+            while (next != null){
+                ret = ret + "\n" + next;
+                next = br.readLine();
+            }
+            br.close();
+        } catch(Exception e) {e.printStackTrace();}
+        return ret;
+
+    }
+    private String assembleCurlCommand(String url, String method){
+        String ret = "curl ";
+        ret = ret + "-X "+ method + " ";
+        ret = ret + url + " ";
+        return ret;
+
+
+    }
 }
